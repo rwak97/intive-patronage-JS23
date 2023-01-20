c995e2e0dc90a9adecc668379890e4a795dcd81c
Rafal_Waraksa <rafal.waraksa1997@gmail.com>
2023-01-20 23:33:17 +0100

    table-data-presentation-fix

--- js/table.js
+++ js/table.js
@@ -1,48 +1,56 @@
 async function loadIntoTable(data, table) {
 	const tableHead = table.querySelector("thead");
 	const tableBody = table.querySelector("tbody");	
-	const response = await fetch("https://api.npoint.io/38edf0c5f3eb9ac768bd");
-	const { transactionTypes, transactions } = await response.json();
+	const url = "https://api.npoint.io/38edf0c5f3eb9ac768bd";
+	const { transactionTypes, transactions } = await fetchData(url);
 
 	// populate the rows
 	for (const row of transactions) {
 		const rowElement = document.createElement("tr");
 
-		for (const [key, cellText] of Object.entries(row)) {
-			const cellElement = document.createElement("td");
-			
-			console.log(key, cellText);
-			var cellTypeContent = "something wrong"
-			if (key == "type") {
-				if (cellText == 1) {
-					cellTypeContent = "<img alt= 'icon1' width=30 height=30 src='pictures/anymoney.jpg' />";
-				} else if (cellText == 2) {
-					cellTypeContent = "<img alt= 'icon2' width=30 height=30 src='pictures/shopping.jpg' />";
-				} else if (cellText == 3) {
-					cellTypeContent = "<img alt= 'icon3' width=30 height=30 src='pictures/salary.jpg' />";
-				} else if (cellText == 4) {
-					cellTypeContent = "<img alt= 'icon4' width=30 height=30 src='pictures/spending.jpg' />";
-				} else {
-					cellTypeContent = cellText;
-				}
-			} else {
-				cellTypeContent = cellText;
-			}
-			cellElement.innerHTML = cellTypeContent;
-			rowElement.appendChild(cellElement);
+		const cellElement1 = document.createElement("td");
+		const cellElement2 = document.createElement("td");
+		const cellElement3 = document.createElement("td");
+		const cellElement4 = document.createElement("td");
+		const cellElement5 = document.createElement("td");
+		
+		var cellTypeContent = "something wrong"
+		
+		const cellText = row.type;
 
+		if (cellText == 1) {
+			cellTypeContent = "<img alt= 'icon1' width=30 height=30 src='pictures/anymoney.jpg' />";
+		} else if (cellText == 2) {
+			cellTypeContent = "<img alt= 'icon2' width=30 height=30 src='pictures/shopping.jpg' />";
+		} else if (cellText == 3) {
+			cellTypeContent = "<img alt= 'icon3' width=30 height=30 src='pictures/salary.jpg' />";
+		} else if (cellText == 4) {
+			cellTypeContent = "<img alt= 'icon4' width=30 height=30 src='pictures/spending.jpg' />";
+		} else {
+			cellTypeContent = cellText;
 		}
+		
+		cellElement1.innerHTML = row.date;
+		cellElement2.innerHTML = cellTypeContent;
+		cellElement3.innerHTML = row.description;
+		cellElement4.innerHTML = row.amount;
+		cellElement5.innerHTML = row.balance;
+
+		rowElement.appendChild(cellElement1);
+		rowElement.appendChild(cellElement2);
+		rowElement.appendChild(cellElement3);
+		rowElement.appendChild(cellElement4);
+		rowElement.appendChild(cellElement5);
 
 		tableBody.appendChild(rowElement);
 	}
 }
 
 async function fetchData(url) {
-	var response = await fetch("https://api.npoint.io/38edf0c5f3eb9ac768bd");
+	var response = await fetch(url);
 	var res = await response.json();
-	 
-	return res;
 
+	return res;
 }
 
 loadIntoTable("res", document.querySelector("table"));
